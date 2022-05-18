4f26f5f77cee07c6cc568bf8b58a88a7cb60fd86
melancholiai <samit2310@gmail.com>
2022-05-18 16:48:38 +0300

    build(deps): upgrade pino-http

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
-import * as PinoHttp from 'pino-http';
-import { Options as PinoHttpOptions, HttpLogger } from 'pino-http';
+import { pinoHttp, Options as PinoHttpOptions, HttpLogger } from 'pino-http';
 import statusCodes from 'http-status-codes';
 import { Logger } from 'pino';
 
 type Options = { logger: Logger; ignorePaths?: string[] } & Pick<PinoHttpOptions, 'customLogLevel' | 'customErrorMessage' | 'customSuccessMessage'>;
 
 const basePinoHttpOptions: PinoHttpOptions = {
-  // err type is wrong
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  customLogLevel: (res, err) => (err !== undefined || res.statusCode >= statusCodes.BAD_REQUEST ? 'error' : 'info'),
+  customLogLevel: (req, res, err) =>
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    err !== undefined || (res.statusCode !== undefined && res.statusCode >= statusCodes.BAD_REQUEST) ? 'error' : 'info',
 };
 
 const httpLogger = (options?: Options): HttpLogger => {
   const { ignorePaths, ...httpOptions } = { ...basePinoHttpOptions, ...options };
   httpOptions.autoLogging = { ignorePaths };
-  return PinoHttp(httpOptions);
+  return pinoHttp(httpOptions);
 };
 
 export { Options };
