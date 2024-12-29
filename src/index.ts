2adf51730621bf107e1b87aaa7e28c251585db2d
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2024-12-29 16:32:11 +0200

    docs: added typedoc

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,35 @@ import { pinoHttp, Options as PinoHttpOptions, HttpLogger, AutoLoggingOptions } 
 import statusCodes from 'http-status-codes';
 import { Logger } from 'pino';
 
-type Options = { logger: Logger; ignorePaths?: (string | RegExp)[]; ignore?: AutoLoggingOptions['ignore'] } & Pick<
-  PinoHttpOptions,
-  'customLogLevel' | 'customErrorMessage' | 'customSuccessMessage' | 'customSuccessObject' | 'customErrorObject'
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+  // eslint-disable-next-line @typescript-eslint/ban-types
+} & {};
+
+/**
+ * Options for configuring the access log middleware.
+ * @property customLogLevel - Custom function to determine log level based on request, response and error.
+ * @property {PinoHttpOptions['customErrorMessage']} [customErrorMessage] - Custom function to generate error messages.
+ * @property {PinoHttpOptions['customSuccessMessage']} [customSuccessMessage] - Custom function to generate success messages.
+ * @property {PinoHttpOptions['customSuccessObject']} [customSuccessObject] - Custom function to modify the success log object.
+ * @property {PinoHttpOptions['customErrorObject']} [customErrorObject] - Custom function to modify the error log object.
+ * @interface
+ */
+type Options = Prettify<
+  {
+    /**
+     * A Logger instance used for logging requests and responses.
+     */
+    logger: Logger;
+    /**
+     * Array of paths or regular expressions to ignore from logging.
+     */
+    ignorePaths?: (string | RegExp)[];
+    /**
+     * Custom ignore options for automatic logging.
+     */
+    ignore?: AutoLoggingOptions['ignore'];
+  } & Pick<PinoHttpOptions, 'customLogLevel' | 'customErrorMessage' | 'customSuccessMessage' | 'customSuccessObject' | 'customErrorObject'>
 >;
 
 const ignorePathFunc = (ignoredPaths: (string | RegExp)[]): AutoLoggingOptions['ignore'] => {
@@ -28,7 +54,21 @@ const basePinoHttpOptions: PinoHttpOptions = {
     err !== undefined || (res.statusCode !== undefined && res.statusCode >= statusCodes.BAD_REQUEST) ? 'error' : 'info',
 };
 
-const httpLogger = (options?: Options): HttpLogger => {
+/**
+ * Creates an HTTP logger middleware for Express using pino-http
+ *
+ * @param options - Configuration options for the HTTP logger
+ * @returns Express middleware that logs HTTP requests and responses
+ *
+ * @example
+ * ```ts
+ * app.use(httpLogger({
+ *   ignorePaths: ['/health', '/metrics'],
+ *   // other pino-http options
+ * }));
+ * ```
+ */
+function httpLogger(options?: Options): HttpLogger {
   const { ignorePaths, ...httpOptions } = { ...basePinoHttpOptions, ...options };
   let ignore: AutoLoggingOptions['ignore'] | undefined = undefined;
 
@@ -42,7 +82,7 @@ const httpLogger = (options?: Options): HttpLogger => {
 
   httpOptions.autoLogging = { ignore };
   return pinoHttp(httpOptions);
-};
+}
 
 export type { Options };
 export default httpLogger;
